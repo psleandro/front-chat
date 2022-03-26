866b0fb243004ff1589fc37d18a3ecff2b68100f
Leandro Santos <lsaantos@outlook.com>
2022-03-26 18:39:09 -0300

    feat: add socket.io and methods

--- src/components/Room/Chat/index.tsx
+++ src/components/Room/Chat/index.tsx
@@ -1,113 +1,33 @@
+import { useEffect, useMemo, useState } from 'react';
+import { io } from 'socket.io-client';
+import { v4 as newUuid } from 'uuid';
 import { Input, Button, Drawer, Avatar, Comment, Typography } from 'antd';
 import {
   VerticalRightOutlined,
   VerticalLeftOutlined,
   CaretRightOutlined,
 } from '@ant-design/icons';
-import { useState } from 'react';
+
 import * as S from './styles';
 
-const arrayMessages = [
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-02',
-    username: 'Gabriel Sartorato',
-    sent_at: '15:59',
-    message: 'Helooo!',
-    color: '#1890ff',
-  },
-  {
-    userId: 'user-03',
-    username: 'Felipe',
-    sent_at: '15:59',
-    message: 'Nice chat guys',
-    color: '#87d068',
-  },
-  {
-    userId: 'user-04',
-    username: 'Luis',
-    sent_at: '15:59',
-    message: 'Cara nada ver isso ai',
-    color: '#34c6eb',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-  {
-    userId: 'user-01',
-    username: 'Leandro Santos',
-    sent_at: '15:59',
-    message:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure)',
-    color: '#f56a00',
-  },
-];
-
-function SendMessage() {
+function SendMessage({ socket }) {
   const [message, setMessage] = useState('');
 
   const handleSendMessage = () => {
     if (!message) return;
+
+    const sent_at = new Date().toLocaleTimeString();
+    const messageObject = {
+      userId: 'Leandro',
+      username: 'Leandro',
+      color: '#f56a00',
+      messageId: newUuid(),
+      message,
+      sent_at,
+    };
+
+    socket.emit('sendMessage', messageObject);
+
     setMessage('');
   };
 
@@ -133,11 +53,24 @@ function SendMessage() {
   );
 }
 
-function Messages() {
+function Messages({ socket }) {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('all messages history', msgData => {
+      setMessages(msgData);
+    });
+
+    socket.on('received message', msg => {
+      setMessages(msgs => [...msgs, msg]);
+    });
+  }, []);
+
   return (
     <S.ChatMessages>
-      {arrayMessages.map(m => (
+      {messages.map(m => (
         <Comment
+          key={m.messageId}
           author={<Typography.Text strong>{m.username}</Typography.Text>}
           avatar={
             <Avatar style={{ backgroundColor: m.color }}>{m.username}</Avatar>
@@ -155,6 +88,16 @@ function Messages() {
 export function Chat() {
   const [showChat, setShowChat] = useState(true);
 
+  const socket = useMemo(() => io('http://localhost:5000'), []);
+
+  useEffect(() => {
+    socket.emit('join room', {
+      userId: 'Leandro',
+      username: 'Leandro',
+      socketId: socket.id,
+    });
+  }, []);
+
   return (
     <>
       <S.IconButton type="text" onClick={() => setShowChat(v => !v)}>
@@ -167,11 +110,11 @@ export function Chat() {
         onClose={() => setShowChat(v => !v)}
         closeIcon={<VerticalLeftOutlined />}
         bodyStyle={{ padding: '0px' }}
-        footer={<SendMessage />}
+        footer={<SendMessage socket={socket} />}
         mask={false}
       >
         <S.ChatContent>
-          <Messages />
+          <Messages socket={socket} />
         </S.ChatContent>
       </Drawer>
     </>
