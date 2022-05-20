5e74f29e51915fbc1e5f2232e27ddea02f0a4619
João Luiz <jleaopitta@gmail.com>
2022-05-20 12:35:49 -0300

    fix(microsoft auth): image request on page reload

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 import {
   getAuth,
   GoogleAuthProvider,
@@ -8,7 +9,7 @@ import {
 } from 'firebase/auth';
 import { createContext, useContext, useEffect, useState } from 'react';
 import axios from 'axios';
-import { destroyCookie, setCookie } from 'nookies';
+import { destroyCookie, parseCookies, setCookie } from 'nookies';
 import { AuthContextData, AuthProviderProps, IUser } from '../interfaces';
 
 export const AuthContext = createContext({} as AuthContextData);
@@ -76,8 +77,20 @@ export function AuthProvider({ children }: AuthProviderProps) {
         setCookie(
           undefined,
           '@audio-meet/accessToken',
-          // eslint-disable-next-line dot-notation
           response.user['accessToken'],
+          {
+            path: '/',
+            maxAge: 60 * 60, // 1h,
+          }
+        );
+
+        setCookie(
+          undefined,
+          '@audio-meet/microsoftOAuthToken',
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore-line
+          // eslint-disable-next-line no-underscore-dangle
+          response._tokenResponse.oauthAccessToken,
           {
             path: '/',
             maxAge: 60 * 60, // 1h,
@@ -124,7 +137,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, userResponse => {
+    const unsubscribe = onAuthStateChanged(auth, async userResponse => {
       if (userResponse) {
         const { displayName, uid, photoURL } = userResponse;
 
@@ -135,21 +148,67 @@ export function AuthProvider({ children }: AuthProviderProps) {
         setUser({
           userId: uid,
           name: displayName,
-          image: photoURL,
-          // eslint-disable-next-line dot-notation
           provider: userResponse.providerData[0]['providerId'],
         });
 
-        setCookie(
-          undefined,
-          '@audio-meet/accessToken',
-          // eslint-disable-next-line dot-notation
-          userResponse['accessToken'],
-          {
-            path: '/',
-            maxAge: 60 * 60, // 1h,
+        if (userResponse.providerData[0]['providerId'] === 'google.com') {
+          setUser(value => ({
+            ...value,
+            image: photoURL,
+          }));
+
+          setCookie(
+            undefined,
+            '@audio-meet/accessToken',
+            userResponse['accessToken'],
+            {
+              path: '/',
+              maxAge: 60 * 60, // 1h,
+            }
+          );
+          return;
+        }
+
+        const cookies = parseCookies(undefined);
+        const oAuthCookie = cookies['@audio-meet/microsoftOAuthToken'];
+
+        try {
+          const photo = await axios.get(
+            'https://graph.microsoft.com/v1.0/me/photo/$value',
+            {
+              headers: {
+                Authorization: `Bearer ${oAuthCookie}`,
+              },
+              responseType: 'arraybuffer',
+            }
+          );
+
+          const avatar = Buffer.from(photo.data, 'binary').toString('base64');
+
+          setUser(value => ({
+            ...value,
+            image: avatar,
+          }));
+
+          setCookie(
+            undefined,
+            '@audio-meet/accessToken',
+            userResponse['accessToken'],
+            {
+              path: '/',
+              maxAge: 60 * 60, // 1h,
+            }
+          );
+        } catch (e) {
+          // eslint-disable-next-line no-cond-assign, no-constant-condition
+          if ((e.response.status = 404)) {
+            setUser(value => ({
+              ...value,
+              image: '',
+            }));
           }
-        );
+          console.log(e);
+        }
       } else {
         setUser(undefined);
         destroyCookie(undefined, '@audio-meet/accessToken', {
